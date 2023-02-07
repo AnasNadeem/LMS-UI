14329a174c124183108e8cd5482a7c0b3169d035
Anas Nadeem <anas5678go@gmail.com>
2023-02-07 07:54:38 +0530

    LeadFilterFormRow component added

--- src/Pages/LeadFilterForm.js
+++ src/Pages/LeadFilterForm.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
+import LeadFilterFormRow from "../components/LeadFilterFormRow";
 // import { leadFilterApi } from "../api";
 
 const LeadFilterForm = () => {
     const [filterData, setFilterData] = useState({});
-    const [slugFilter, setSlugFilter] = useState('');
-    const [opFilter, setOpFilter] = useState('');
-    const [valueFilter, setValueFilter] = useState('');
+    const [filterFormRow, setFilterFormRow] = useState([]);
 
     const leadattribute = JSON.parse(localStorage.getItem('leadattribute'));
     const constants = JSON.parse(localStorage.getItem('constants'));
@@ -18,57 +17,21 @@ const LeadFilterForm = () => {
 
     const handleLeadFilter = async (e) => {
         e.preventDefault();
-        const filter = {};
-        filter[slugFilter] = [opFilter, valueFilter];
-        console.log(filter);
+        // const filter = {};
+        // filter[slugFilter] = [opFilter, valueFilter];
+        // console.log(filter);
     }
 
     return (
         <form onSubmit={handleLeadFilter}>
             <div className="card card-body">
+                <LeadFilterFormRow leadattribute={leadattribute} leadFullAttrObj={leadFullAttrObj} filterCombo={filterCombo}/>
                 <div className="row">
-                    <div className="col-4">
-                        <div className="d-flex FormCardBodyGroupInput rounded">
-                        <select
-                        className="form-select border-0"
-                        value={slugFilter}
-                        onChange={(e) => setSlugFilter(e.target.value)}
-                        >
-                            {leadattribute.map((leadAttr) => (
-                                <option key={leadAttr.slug} value={leadAttr.slug}>{leadAttr.name}</option>
-                            ))}
-                        </select>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div className="d-flex FormCardBodyGroupInput rounded">
-                        <select
-                        className="form-select border-0"
-                        value={opFilter}
-                        onChange={(e) => setOpFilter(e.target.value)}
-                        >
-                            {slugFilter ?
-                                filterCombo[leadFullAttrObj[slugFilter].attribute_type].map((filter) => (
-                                    <option key={filter} value={filter}>{filter}</option>
-                                ))
-                            : <option value="0">select the attribute</option>
-                            }
-                        </select>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div className="d-flex FormCardBodyGroupInput rounded">
-                            <input
-                            type="text"
-                            className="form-control border-0"
-                            id="name"
-                            value={valueFilter}
-                            onInput={(e) => setValueFilter(e.target.value)}
-                            />
-                        </div>
+                    <div className="col-4 offset-8">
+                        <button className="btn btn-primary FormCardBodyGroupButton mt-2">Add Filter</button>
                     </div>
                 </div>
-                <button className="btn btn-primary FormCardBodyGroupButton mt-2" type="submit">Submit</button>
+                    <button className="btn btn-primary FormCardBodyGroupButton mt-2" type="submit">Submit</button>
             </div>
         </form>
     )
---
+++ src/components/LeadFilterFormRow.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const LeadFilterFormRow = ({leadattribute, leadFullAttrObj, filterCombo}) => {
+    const [slugFilter, setSlugFilter] = useState('');
+    const [opFilter, setOpFilter] = useState('');
+    const [valueFilter, setValueFilter] = useState('');
+
+    return (
+        <div className="row">
+            <div className="col-4">
+                <div className="d-flex FormCardBodyGroupInput rounded">
+                <select
+                className="form-select border-0"
+                value={slugFilter}
+                onChange={(e) => setSlugFilter(e.target.value)}
+                >
+                    {leadattribute.map((leadAttr) => (
+                        <option key={leadAttr.slug} value={leadAttr.slug}>{leadAttr.name}</option>
+                    ))}
+                </select>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="d-flex FormCardBodyGroupInput rounded">
+                <select
+                className="form-select border-0"
+                value={opFilter}
+                onChange={(e) => setOpFilter(e.target.value)}
+                >
+                    {slugFilter ?
+                        filterCombo[leadFullAttrObj[slugFilter].attribute_type].map((filter) => (
+                            <option key={filter} value={filter}>{filter}</option>
+                        ))
+                    : <option value="0">select the attribute</option>
+                    }
+                </select>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="d-flex FormCardBodyGroupInput rounded">
+                    <input
+                    type="text"
+                    className="form-control border-0"
+                    id="name"
+                    value={valueFilter}
+                    onInput={(e) => setValueFilter(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LeadFilterFormRow;
